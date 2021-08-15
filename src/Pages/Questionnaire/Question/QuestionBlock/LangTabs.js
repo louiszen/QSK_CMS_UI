99c6674f3d24f4abf31c1a5245c9ff014fc96768
Louiszen <louiszen@gmail.com>
2021-08-15 16:48:05 +0800

    pos

---
+++ src/Pages/Questionnaire/Question/QuestionBlock/LangTabs.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import { Accessor } from '@IZOArc/STATIC';
+import PropsType from 'prop-types';
+import _ from 'lodash';
+import { Box } from '@material-ui/core';
+import { StyledButton } from '@IZOArc/LabIZO/Stylizo';
+import { HStack } from '@IZOArc/LabIZO/Stackizo';
+
+/**
+ * @augments {Component<Props, State>}
+ */
+class LangTabs extends Component {
+
+  static propTypes = {
+    langs: PropsType.arrayOf(PropsType.string),
+    func: PropsType.func
+  }
+
+  static defaultProps = {
+    langs: ["EN", "TC", "SC"],
+    func: () => {}
+  }
+
+  constructor(){
+    super();
+    this.state = {
+      value: "EN"
+    };
+  }
+
+  componentDidMount(){
+    this._setAllStates();
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(!Accessor.IsIdentical(prevProps, this.props, Object.keys(LangTabs.defaultProps))){
+      this._setAllStates();
+    }
+  }
+
+  componentWillUnmount() {
+    this.setState = (state, callback) => {
+        return;
+    };
+  }
+
+  _setAllStates = (callback) => {
+    this.setState((state, props) => ({
+      ...props,
+    }), callback);
+  }
+
+  onChangeTab = (tab) => {
+    this.setState({
+      value: tab
+    });
+  }
+
+  renderTabs(){
+    let {langs} = this.props;
+    let {value} = this.state;
+    let ontheme = {
+      height: 30,
+      color: "blue",
+      borderRadius: "0px"
+    };
+    let offtheme = {
+      height: 30,
+      color: "grey",
+      borderRadius: "0px"
+    };
+    return _.map(langs, (o, i) => {
+      return (
+        <StyledButton onClick={() => this.onChangeTab(o)} theme={value === o? ontheme : offtheme}>
+          {o}
+        </StyledButton>
+      )
+    })
+  }
+
+  renderTabPanel(){
+    let {value} = this.state;
+    let {langs, func} = this.props;
+    return _.map(langs, (o, i) => {
+      if(value === o && _.isFunction(func)){
+        return func(value);
+      }
+    });
+  }
+
+  render(){
+    return (
+      <Box width="100%" height="100%">
+        <HStack spacing={5} justifyContent="flex-start">
+          {this.renderTabs()}
+        </HStack>
+        {this.renderTabPanel()}
+      </Box>
+    );
+  }
+
+}
+
+export default LangTabs;
--- src/Pages/Questionnaire/Question/QuestionBlock/QuestionBlock.js
+++ src/Pages/Questionnaire/Question/QuestionBlock/QuestionBlock.js
@@ -6,6 +6,7 @@ import { Tooltip, Typography } from '@material-ui/core';
 import _ from 'lodash';
 import { DeleteForever, Edit, InfoOutlined } from '@material-ui/icons';
 import { StyledIconButton } from '@IZOArc/LabIZO/Stylizo';
+import LangTabs from './LangTabs';
 
 /**
  * @augments {Component<Props, State>}
@@ -114,35 +115,190 @@ class QuestionBlock extends Component {
 
   renderQCond(){
     return (
-      <VStack justifyContent="flex-start" width="100%" height={100} style={{background: ColorX.GetBGColorCSS("lightYellow")}}>
+      <VStack justifyContent="flex-start" width="100%" height={200} style={{background: ColorX.GetBGColorCSS("lightYellow")}}>
         <Typography style={{fontSize: 12, width: "100%", fontWeight: "bold"}}>
-          {"Conditions:"}
+          {"Prerequisites:"}
         </Typography>
         {this.renderCondList()}
       </VStack>
     );
   }
 
+  renderQContent(doc, lang){
+    return (
+      <Typography style={{width: "100%"}}>
+        {Accessor.Get(doc, "question.title." + lang)}
+      </Typography>
+    )
+  }
+
   renderQ(){
     let {doc} = this.props;
     return (
       <VStack justifyContent="flex-start" width="100%" height={150} style={{background: ColorX.GetColorCSS("aliceblue")}}>
-        <Typography style={{fontSize: 12, width: "100%", }}>
+        <Typography style={{fontSize: 12, width: "100%", fontWeight: "bold" }}>
           {"Question:"}
         </Typography>
-        <Typography style={{width: "100%"}}>
-          {Accessor.Get(doc, "question.title.EN")}
+        <LangTabs func={(e) => this.renderQContent(doc, e)}/>
+      </VStack>
+    );
+  }
+
+  rednerInduction(){
+    let {doc} = this.props;
+    let choices = Accessor.Get(doc, "choices");
+    return (
+      <VStack width="100%">
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Induction Method:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(choices, "induction")}
+          </Typography>
+        </HStack>
+      </VStack>
+    );
+  }
+
+  rednerMappingExtDB(){
+    let {doc} = this.props;
+    let choices = Accessor.Get(doc, "choices");
+    if(!choices) return;
+    let mapping = Accessor.Get(choices, "mapping");
+    if(!mapping) return;
+    return (
+      <VStack width="100%">
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Database Name:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(mapping, "dbName")}
+          </Typography>
+        </HStack>
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Linked field:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(mapping, "link")}
+          </Typography>
+        </HStack>
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Linked value:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(mapping, "value")}
+          </Typography>
+        </HStack>
+      </VStack>
+    );
+  }
+
+  renderExternalDB(){
+    let {doc} = this.props;
+    let choices = Accessor.Get(doc, "choices");
+    if(!choices) return;
+    return (
+      <VStack width="100%">
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Database Name:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(choices, "dbName")}
+          </Typography>
+        </HStack>
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Display field:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(choices, "display")}
+          </Typography>
+        </HStack>
+      </VStack>
+    );
+  }
+
+  renderChoice(){
+    let {doc} = this.props;
+    return (
+      <VStack width="100%" spacing={5}>
+        <Typography style={{fontSize: 12, width: "100%", fontWeight: "bold" }}>
+          {"Choices Settings:"}
         </Typography>
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Multi-Select:"}
+          </Typography>
+          
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(doc, "choices.multiSelect")? "YES" : "NO"}
+          </Typography>
+        </HStack>
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%", fontStyle: "italic", fontWeight: "bold"}}>
+            {"Apply induction?:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(doc, "choices.applyInduction")? "YES" : "NO"}
+          </Typography>
+        </HStack>
+        {Accessor.Get(doc, "choices.applyInduction") === true && this.rednerInduction()}
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {"Choice List:"}
+          </Typography>
+        </HStack>
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%", fontStyle: "italic", fontWeight: "bold"}}>
+            {"Using External DB?:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(doc, "choices.extDB")? "YES" : "NO"}
+          </Typography>
+        </HStack>
+        {Accessor.Get(doc, "choices.extDB") === true && this.renderExternalDB()}
+        <HStack>
+          <Typography style={{fontSize: 12, width: "100%", fontStyle: "italic", fontWeight: "bold"}}>
+            {"Apply mapping?:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%"}}>
+            {Accessor.Get(doc, "choices.applyMapping")? "YES" : "NO"}
+          </Typography>
+        </HStack>
+        {Accessor.Get(doc, "choices.mapping.extDB") === true && this.rednerMappingExtDB()}
+        
+      </VStack>
+    );
+  }
+
+  renderAnswer(){
+    let {doc} = this.props;
+    return (
+      <VStack width="100%" height="fit-content">
+        <HStack spacing={5}>
+          <Typography style={{fontSize: 12, width: "100%", fontWeight: "bold" }}>
+            {"Answer Type:"}
+          </Typography>
+          <Typography style={{fontSize: 12, width: "100%" }}>
+            {Accessor.Get(doc, "variant")}
+          </Typography>
+        </HStack>
+        {Accessor.Get(doc, "variant") === "select" && this.renderChoice()}
       </VStack>
     );
   }
 
   renderQuestion(){
-    let {doc} = this.props;
     return (
       <VStack justifyContent="flex-start" width="100%">
         {this.renderQCond()}
         {this.renderQ()}
+        {this.renderAnswer()}
       </VStack>
     )
   }
