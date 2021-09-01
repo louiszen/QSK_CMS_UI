967813c85a0a2e0d964bc8ba00f14a24df4b2535
louiszen <louiszen.yip@mobinology.com>
2021-09-01 17:48:01 +0800

    IZOArc & package

--- src/Pages/Severity/Grouping/Grouping.js
+++ src/Pages/Severity/Grouping/Grouping.js
@@ -59,7 +59,16 @@ class Grouping extends Component {
           fail: "Location Severity Add Failed: ",
           schema: schema.Add,
           buttons: ["Clear", "Submit"],
-          onSubmit: "Add"
+          onSubmit: "Add",
+          defaultDoc: {
+            lastUpdate: "2021-06-02T16:00:00.000Z",
+            location: "Mainland China",
+            group: "Mainland China",
+            effective: {
+              Start: "2021-06-02T16:00:00.000Z",
+              End: null
+            }
+          }
         },
         Delete: {
           title: "Delete this Location Severity?",
