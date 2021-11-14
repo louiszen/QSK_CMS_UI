69273c32999b2c6f81b9b3ae6febd5ef36795530
Louiszen <louiszen@gmail.com>
2021-11-14 23:12:52 +0800

    IZOArc

---
+++ src/__SYSDefault/SysAPI.js
@@ -0,0 +1,4 @@
+export const CheckUserNameAPI = "/CommonAPI/Auth/CheckUserName";
+export const SignInAPI = "/CommonAPI/Auth/SignIn";
+export const EnvInfoAPI = "/CommonAPI/Env/Info";
+export const CheckInitAPI = "/CommonAPI/Env/IsInitialized";
