da2ed53108919f9ecf9624f0ef6b314158391fce
Louiszen <louiszen@gmail.com>
2022-03-02 13:21:48 +0800

    dummy

--- src/Mods/PermitForm/index.js
+++
@@ -1,9 +0,0 @@
-import F416 from "./F416";
-import HotWork from "./HotWork";
-
-let PermitForm = {
-  F416,
-  HotWork
-};
-
-export default PermitForm;
--- src/Mods/PermitTable/index.js
+++
@@ -1,9 +0,0 @@
-import F416 from "./F416";
-import HotWork from "./HotWork";
-
-let PermitTable = {
-  F416,
-  HotWork
-};
-
-export default PermitTable;
